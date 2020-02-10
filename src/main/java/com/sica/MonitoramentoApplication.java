ac875c6ee75bf2949ac8f090b4d8bab676bee6e4
tarcisio.filo2 <tarcisio.filo2@gmail.com>
2020-02-10 12:51:00 -0300

    teste

--- src/main/java/com/sica/MonitoramentoApplication.java
+++ src/main/java/com/sica/MonitoramentoApplication.java
@@ -1,12 +1,19 @@
 package com.sica;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 public class MonitoramentoApplication {
+
+	@Bean
+	public CommandLineRunner runner() {
+		return new RabbitAmqpRunner();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonitoramentoApplication.class, args);
